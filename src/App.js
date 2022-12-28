f312567e622bfba890f9a999bfaca101ec3591ff
rptomar <rohitpal23478@gmail.com>
2022-12-28 03:08:52 -0800

    files are added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Signin from './components/Registeration/Signin';
+import Login from './components/Registeration/Login'
+
+function App() {
+  return (
+    <div >
+      <Signin/>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
